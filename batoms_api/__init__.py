f00504d9aaefb009260b5f5024683e59fb4a31f7
T.Tian <alchem0x2a@gmail.com>
2022-06-15 15:50:36 -0400

    add post_modification method

--- batoms_api/__init__.py
+++ batoms_api/__init__.py
@@ -1,22 +1,35 @@
 import pickle
 import os
 
-def render(atoms, batoms_input = {}, render_input = {}, display = False, queue = None, ):
-    with open('.batoms.inp', 'wb') as f:
-        pickle.dump([atoms, batoms_input, render_input], f)
+
+def render(
+    atoms,
+    batoms_input={},
+    render_input={},
+    display=False,
+    queue=None,
+    post_modifications=[],
+):
+    """
+    atoms: an ASE atoms object
+    batoms_input: input parameters to create the Batoms object
+    render_input: input parameters to create the Render object
+    post_modifications: list of commands to be evaluated following the given sequences
+    """
+    with open(".batoms.inp", "wb") as f:
+        pickle.dump([atoms, batoms_input, render_input, post_modifications], f)
     #
-    blender_cmd = 'blender'
-    if 'BLENDER_COMMAND' in os.environ.keys():
-        blender_cmd = os.environ['BLENDER_COMMAND']
+    blender_cmd = "blender"
+    if "BLENDER_COMMAND" in os.environ.keys():
+        blender_cmd = os.environ["BLENDER_COMMAND"]
     root = os.path.normpath(os.path.dirname(__file__))
-    script = os.path.join(root, 'script-api.py')
+    script = os.path.join(root, "script-api.py")
     if display:
-        cmd = blender_cmd + ' -P ' + script
-    elif queue == 'SLURM':
-        cmd = 'srun -n $SLURM_NTASKS ' +  blender_cmd + ' -b ' + ' -P ' + script
+        cmd = blender_cmd + " -P " + script
+    elif queue == "SLURM":
+        cmd = "srun -n $SLURM_NTASKS " + blender_cmd + " -b " + " -P " + script
     else:
-        cmd = blender_cmd + ' -b ' + ' -P ' + script
+        cmd = blender_cmd + " -b " + " -P " + script
     errcode = os.system(cmd)
     if errcode != 0:
-        raise OSError('Command ' + cmd +
-                      ' failed with error code %d' % errcode)
+        raise OSError("Command " + cmd + " failed with error code %d" % errcode)
--- batoms_api/script-api.py
+++ batoms_api/script-api.py
@@ -1,13 +1,28 @@
 from batoms.batoms import Batoms
+import numpy as np
 import pickle
+blender_globals = globals().copy()
+
 
 def run():
-    with open('.batoms.inp', 'rb') as f:
-        atoms, batoms_input, render_input = pickle.load(f)
-        if 'label' not in batoms_input:
-            batoms_input['label'] = 'batoms'
-        batoms = Batoms(from_ase = atoms, **batoms_input)
+    """post_modifications are like `ase run --modify` parameters that are direct python expressions (use at your own risk!)
+    format of post_modifications looks like follows:
+        - batoms.<property>.<sub_property><[indices/keys]> = something
+    each post-modification line is directly evaluated in sequence.
+    """
+    with open(".batoms.inp", "rb") as f:
+        atoms, batoms_input, render_input, post_modifications = pickle.load(f)
+        if "label" not in batoms_input:
+            batoms_input["label"] = "batoms"
+        batoms = Batoms(from_ase=atoms, **batoms_input)
+        for mod in post_modifications:
+            # TODO: sanity check of expression?
+            blender_globals.update({"batoms": batoms, "np": np})
+            exec(mod, blender_globals)
         batoms.get_image(**render_input)
+        return
+            
+
 
 if __name__ == "__main__":
     run()
