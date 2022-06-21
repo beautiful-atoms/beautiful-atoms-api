ebccc38db11ea02e94e0fc61d67520d9306fd1f2
T.Tian <alchem0x2a@gmail.com>
2022-06-21 16:14:55 -0400

    tests for args handler

--- tests/api/test_script_api.py
+++ tests/api/test_script_api.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import patch
 
 
 @pytest.mark.filterwarnings("error:Input file api")
@@ -14,3 +15,26 @@ def test_version_check():
         _check_version("0.1.0")
     # same version
     _check_version(__version__)
+
+def test_args_handler():
+    import sys
+    from batoms_api.script_api import _handle_argv_extras
+    blender_args_base = ["blender", "-b", "--python-expr", "from batoms_api import script_api; script_api.run()",]
+    # No additional parameter provided, return None
+    with patch.object(sys, "argv", blender_args_base):
+        assert _handle_argv_extras() is None
+    # Only add "--" without extra args
+    with patch.object(sys, "argv", blender_args_base + ["--"]):
+        with pytest.raises(ValueError):
+            _handle_argv_extras()
+    # More args than needed
+    with patch.object(sys, "argv", blender_args_base + ["--", ".batoms.inp", "a", "b"]):
+        with pytest.raises(ValueError):
+            _handle_argv_extras()
+    # Exactly one arg
+    with patch.object(sys, "argv", blender_args_base + ["--", ".batoms.inp"]):
+        filename = _handle_argv_extras()
+        assert filename == ".batoms.inp"
+    
+        
+    
