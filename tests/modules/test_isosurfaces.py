118150404c0728029cb88c61c3d815429e78202e
T.Tian <alchem0x2a@gmail.com>
2022-06-23 17:37:23 -0400

    add multilayer isosurface test

--- tests/modules/test_isosurfaces.py
+++ tests/modules/test_isosurfaces.py
@@ -44,6 +44,41 @@ def test_isosurfaces():
     # os.remove(".batoms.blend")
 
 
+def test_isosurfaces_multi():
+    from ase.io.cube import read_cube_data
+    from batoms_api import render
+
+    volume, atoms = read_cube_data(data_path / "h2o-homo.cube")
+    config = base_config.copy()
+    config["settings"].update(
+        {
+            "model_style": 1,
+            "isosurfaces": {"setting": {}},
+        }
+    )
+    levels = [-0.001, -0.005, -0.01, -0.02, -0.05]
+    for i in range(5):
+        config["settings"]["isosurfaces"]["setting"][str(i)] = {"level": levels[i], "color": [1, 1, 0, 0.2]}
+    for i in range(5, 10):
+        config["settings"]["isosurfaces"]["setting"][str(i)] = {"level": -levels[i - 5], "color": [0, 1, 1, 0.2]}
+
+    config["post_modifications"] = ["batoms.isosurfaces.draw()"]
+    render(atoms, volume=volume, save_blender_file=True, **config)
+    with load_blender_file() as do:
+        batoms = do["batoms"]
+        label = batoms.label
+        assert len(batoms.isosurfaces.setting) == 10
+        for i in range(5):
+            iso = batoms.coll.objects[f"{label}_isosurface_{i}"]
+            c = get_material_color(iso)
+            assert np.isclose(c, [1, 1, 0, 0.2]).all()
+        for i in range(5, 10):
+            iso = batoms.coll.objects[f"{label}_isosurface_{i}"]
+            c = get_material_color(iso)
+            assert np.isclose(c, [0, 1, 1, 0.2]).all()
+    os.remove(".batoms.blend")
+
+
 if __name__ == "__main__":
     test_isosurfaces()
     print("\n Isosurfaces: All pass! \n")
